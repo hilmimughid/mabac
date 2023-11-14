7ea8d0ee77364e1ec519608a1feccdcca9a41a8b
hilmimughid <113970790+hilmimughid@users.noreply.github.com>
2023-11-14 23:42:48 +0700

    Add Input Alternatif

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+var namaKriteria = [];
 function inputKriteria() {
     var jumlahKriteria = document.getElementById("jumlahKriteria").value;
     var container = document.getElementById("formKriteria");
@@ -7,13 +8,18 @@ function inputKriteria() {
         var formKriteria = document.createElement("form");
         formKriteria.id = "formKriteria" + i;
         var labelNamaKriteria = document.createElement("label");
-        labelNamaKriteria.innerHTML = "Nama Kriteria " + (i + 1) + ": ";
-        var inputNamaKriteria = document.createElement("input");
-        inputNamaKriteria.type = "text";
+        labelNamaKriteria.innerHTML = "Pilih Kriteria: ";
+        var inputNamaKriteria = document.createElement("select");
         inputNamaKriteria.name = "inputNamaKriteria";
+        for (var j = 0; j < jumlahKriteria; j++) {
+            var option = document.createElement("option");
+            option.value = "C" + (j + 1);
+            option.text = "C" + (j + 1);
+            inputNamaKriteria.appendChild(option);
+        }
         var br1 = document.createElement("br");
         var labelJenisKriteria = document.createElement("label");
-        labelJenisKriteria.innerHTML = "Jenis Kriteria " + (i + 1) + ": ";
+        labelJenisKriteria.innerHTML = "Pilih Jenis Kriteria: ";
         var inputJenisKriteria = document.createElement("select");
         inputJenisKriteria.name = "inputJenisKriteria";
         var option1 = document.createElement("option");
@@ -26,7 +32,7 @@ function inputKriteria() {
         inputJenisKriteria.appendChild(option2);
         var br2 = document.createElement("br");
         var labelNilaiKriteria = document.createElement("label");
-        labelNilaiKriteria.innerHTML = "Nilai Kriteria " + (i + 1) + ": ";
+        labelNilaiKriteria.innerHTML = "Bobot Kriteria: ";
         var inputNilaiKriteria = document.createElement("input");
         inputNilaiKriteria.type = "text";
         inputNilaiKriteria.name = "inputNilaiKriteria";
@@ -47,9 +53,9 @@ function inputKriteria() {
     document.getElementById("submitKriteria").style.display = "block";
 }
 
-function tabelKriteria() {
+function tampilTabelKriteria() {
     var jumlah = document.getElementById("jumlahKriteria").value;
-    var tabelContainer = document.getElementById("tabelContainer");
+    var tabelContainer = document.getElementById("tabelKriteria");
     tabelContainer.innerHTML = "";
 
     var tabel = document.createElement("table");
@@ -94,5 +100,96 @@ function tabelKriteria() {
     tabelContainer.appendChild(tabel);
 
     // Mencegah form submit dikirim
+    return false;
+}
+
+function inputAlternatif() {
+    var jumlahAlternatif = document.getElementById("jumlahAlternatif").value;
+    var jumlahKriteria = document.getElementById("jumlahKriteria").value;
+    var container = document.getElementById("formAlternatif");
+    container.innerHTML = "";
+
+    for (var i = 0; i < jumlahAlternatif; i++) {
+        var formAlternatif = document.createElement("form");
+        formAlternatif.id = "formAlternatif" + i;
+
+        var labelNamaAlternatif = document.createElement("label");
+        labelNamaAlternatif.innerHTML = "Nama Alternatif " + (i + 1) + ": ";
+        var inputNamaAlternatif = document.createElement("select");
+        inputNamaAlternatif.name = "inputNamaAlternatif";
+        for (var j = 0; j < jumlahAlternatif; j++) {
+            var option = document.createElement("option");
+            option.value = "A" + (j + 1);
+            option.text = "A" + (j + 1);
+            inputNamaAlternatif.appendChild(option);
+        }
+        var br1 = document.createElement("br");
+        formAlternatif.appendChild(labelNamaAlternatif);
+        formAlternatif.appendChild(inputNamaAlternatif);
+        formAlternatif.appendChild(br1);
+
+        for (var k = 0; k < jumlahKriteria; k++) {
+            var labelNilaiAlternatif = document.createElement("label");
+            labelNilaiAlternatif.innerHTML = "Nilai A" + (k + 1) + " pada C" + (k + 1) + ":";
+            var inputNilaiAlternatif = document.createElement("input");
+            inputNilaiAlternatif.type = "text";
+            inputNilaiAlternatif.name = "inputNilaiAlternatif" + k;
+            var br2 = document.createElement("br");
+            formAlternatif.appendChild(labelNilaiAlternatif);
+            formAlternatif.appendChild(inputNilaiAlternatif);
+            formAlternatif.appendChild(br2);
+        }
+
+        container.appendChild(formAlternatif);
+    }
+
+    // Tampilkan form submit
+    document.getElementById("submitAlternatif").style.display = "block";
+}
+
+function tampilTabelAlternatif() {
+    var jumlahAlternatif = document.getElementById("jumlahAlternatif").value;
+    var jumlahKriteria = document.getElementById("jumlahKriteria").value;
+    var container = document.getElementById("tabelAlternatif");
+    container.innerHTML = "";
+
+    var table = document.createElement("table");
+    table.className = "bordered-table";
+    var thead = document.createElement("thead");
+    var tbody = document.createElement("tbody");
+
+    // Membuat header tabel
+    var tr = document.createElement("tr");
+    var th = document.createElement("th");
+    th.innerHTML = "";
+    tr.appendChild(th);
+    for (var i = 0; i < jumlahKriteria; i++) {
+        var th = document.createElement("th");
+        var inputNamaKriteria = document.getElementById("formKriteria" + i).elements["inputNamaKriteria"];
+        th.innerHTML = inputNamaKriteria.options[inputNamaKriteria.selectedIndex].text;
+        tr.appendChild(th);
+    }
+    thead.appendChild(tr);
+
+    // Membuat isi tabel
+    for (var i = 0; i < jumlahAlternatif; i++) {
+        var tr = document.createElement("tr");
+        var td = document.createElement("td");
+        var inputNamaAlternatif = document.getElementById("formAlternatif" + i).elements["inputNamaAlternatif"];
+        td.innerHTML = inputNamaAlternatif.options[inputNamaAlternatif.selectedIndex].text;
+        tr.appendChild(td);
+        for (var j = 0; j < jumlahKriteria; j++) {
+            var td = document.createElement("td");
+            var inputNilaiAlternatif = document.getElementById("formAlternatif" + i).elements["inputNilaiAlternatif" + j];
+            td.innerHTML = inputNilaiAlternatif.value;
+            tr.appendChild(td);
+        }
+        tbody.appendChild(tr);
+    }
+
+    table.appendChild(thead);
+    table.appendChild(tbody);
+    container.appendChild(table);
+
     return false;
 }
