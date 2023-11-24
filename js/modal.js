2d57e70364d64c252967c2b753bd08257f01a3d9
hilmimughid <113970790+hilmimughid@users.noreply.github.com>
2023-11-24 10:31:18 +0700

    Fix Modal

---
+++ js/modal.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+    $("#buttonEditKriteria").click(function () {
+        $("#modalEditKriteria").modal("hide");
+    });
+
+    $("#buttonHapusSatuKriteria").click(function () {
+        $("#modalHapusSatuKriteria").modal("hide");
+    });
+
+    $("#buttonTambahKriteria").click(function () {
+        $("#modalTambahKriteria").modal("hide");
+    });
+
+    $("#buttonHapusSemuaKriteria").click(function () {
+        $("#modalHapusSemuaKriteria").modal("hide");
+    });
+});
